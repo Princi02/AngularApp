d69133553dbae802fe2b6f8b6858ca7e332c4338
Princi02 <priyanshimangal891@gmail.com>
2025-03-22 16:28:37 +0530

    open bridgelabz url in new tab using event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angularApp';
   imgUrl ="../assets/BL_logo_square_jpg.jpg";
+  url = "https://www.bridgelabz.com"; // BridgeLabz Website URL
 
   ngOnInit(): void {
     this.title = "Hello from BridgeLabz.";      // it is called after component is initialized, use when dynamic value    
+  }
+
+  onClick(event: Event){
+    console.log("Save button is clicked!", event);
+    window.open(this.url, "_blank");
   }
   // message: string = "Hello from BridgeLabz! 🚀"; // Template Expression Variable,  message is initialized when the component is created,    use when static values
 }
