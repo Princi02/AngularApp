dbbe748c7e636269b63cc98d6e7f7731c8bdf1aa
Princi02 <priyanshimangal891@gmail.com>
2025-03-22 16:59:56 +0530

    user input validation through event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent {
   url = "https://www.bridgelabz.com"; // BridgeLabz Website URL
 
   userName: string = "";
+  nameError: string = "";
 
   ngOnInit(): void {
     this.title = "Hello from BridgeLabz.";      // it is called after component is initialized, use when dynamic value    
@@ -21,4 +22,21 @@ export class AppComponent {
     window.open(this.url, "_blank");
   }
   // message: string = "Hello from BridgeLabz! 🚀"; // Template Expression Variable,  message is initialized when the component is created,    use when static values
+
+
+
+
+  onInput($event: Event) {
+    console.log("Change Event Occurred!", ($event.target as HTMLInputElement).value);
+  
+    this.userName = ($event.target as HTMLInputElement).value;  // Update userName
+    
+    const nameRegex = /^[A-Z]{1}[a-zA-Z\s]{2,}$/;  // Fixed regex
+  
+    if (nameRegex.test(this.userName)) {
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is Incorrect!";
+  }
 }
